b73c148ebba78f2a93961ebd2f7e68563136d557
Matt Pye <pyematt@gmail.com>
2020-06-17 00:53:22 +0100

    Test get_client_ip

---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+from magiclink.utils import get_client_ip
+
+from django.http import HttpRequest
+
+
+def test_get_client_ip_http_x_forwarded_for():
+    request = HttpRequest()
+    ip_addr = '127.0.0.1'
+    request.META['HTTP_X_FORWARDED_FOR'] = f'{ip_addr}, 127.0.0.1'
+    ip_address = get_client_ip(request)
+    assert ip_address == ip_addr
+
+
+def test_get_client_ip_remote_addr():
+    request = HttpRequest()
+    remote_addr = '127.0.0.1'
+    request.META['REMOTE_ADDR'] = remote_addr
+    ip_address = get_client_ip(request)
+    assert ip_address == remote_addr
