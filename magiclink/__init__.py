db1153c7b1234cdd67a255834eaf9bffd4714eca
Matt Pye <pyematt@gmail.com>
2021-05-29 03:00:47 +0100

    Remove default_app_config for Django 3.2 +

--- magiclink/__init__.py
+++ magiclink/__init__.py
@@ -1,1 +1,11 @@
-default_app_config = 'magiclink.apps.MagiclinkConfig'
+from django import get_version
+from packaging import version
+
+"""
+Stop waring:
+    RemovedInDjango41Warning: 'magiclink' defines
+    default_app_config = 'magiclink.apps.MagiclinkConfig'. Django now detects
+    this configuration automatically. You can remove default_app_config.
+"""
+if version.parse(get_version()) < version.parse('3.2'):  # pragma: no cover
+    default_app_config = 'magiclink.apps.MagiclinkConfig'
