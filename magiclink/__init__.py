91db85f01275782679c1cfbdb256b36480a0cb70
Matt Pye <pyematt@gmail.com>
2021-06-06 12:19:54 +0100

    Ensure packaging dependency for version check

--- magiclink/__init__.py
+++ magiclink/__init__.py
@@ -2,7 +2,7 @@ from django import get_version
 from packaging import version
 
 """
-Stop waring:
+To stop waring:
     RemovedInDjango41Warning: 'magiclink' defines
     default_app_config = 'magiclink.apps.MagiclinkConfig'. Django now detects
     this configuration automatically. You can remove default_app_config.
